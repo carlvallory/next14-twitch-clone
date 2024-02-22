eb8ad6dd3cc44ae6ff4fb3e8ad5733fb2b1246a5
cvallory <carlvallory@gmail.com>
2024-02-22 17:00:27 -0300

    Enable NextAuth v0.3.5

--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -1,15 +1,27 @@
-"use client";
-
 import { signOut } from "next-auth/react";
+import { getSelf } from "@/lib/auth-service";
 
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { UserAvatar } from "@/components/user-avatar";
 
-export const UserButton = ({ props }: any) => {
-    const onClick = () => {
-        signOut();
-    }
+export const UserButton = async ({ props }: any) => {
+    const user = await getSelf();
+
+    if(!user) return null;
 
     return(
-        <Button onClick={onClick} size="sm">Logout</Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <UserAvatar 
+                    username={user.username!}
+                    imageUrl={user.image!}
+                />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>{user.username}</DropdownMenuLabel>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        
     );
 };
